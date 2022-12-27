348deb15c0eb754f399e25138e071d1b95d06b5f
WilliamDemo <59418852+shimmytube@users.noreply.github.com>
2022-12-26 22:17:27 -0500

    123

--- 4Stack/EvalPostfix.cpp
+++ 4Stack/EvalPostfix.cpp
@@ -2,63 +2,64 @@
 #include<cmath>
 #include<stack>
 using namespace std;
+//string post = "53+62/*35*+";  //39
 
-float scanNum(char ch) {
-   int value;
-   value = ch;
-   return float(value-'0');   //return float from character
+// The function calculate_Postfix returns the final answer of the expression after calculation
+int calculatePostfix(string post_exp)
+{
+    stack <int> stack;
+    int len = post_exp.length();
+
+    // loop to iterate through the expression
+    for (int i = 0; i < len; i++)
+    {
+        // if the character is an operand we push it in the stack
+        // we have considered single digits only here
+        if ( post_exp[i] >= '0' &&  post_exp[i] <= '9')
+        {
+            stack.push( post_exp[i] - '0');
+        }
+        // if the character is an operator we enter else block
+        else
+        {
+            // we pop the top two elements from the stack and save them in two integers
+            int a = stack.top();
+            stack.pop();
+            int b = stack.top();
+            stack.pop();
+            
+            //performing the operation on the operands
+            switch (post_exp[i])
+            {
+                case '+': // addition
+                          stack.push(b + a);
+                          break;
+                case '-': // subtraction
+                          stack.push(b - a);
+                          break;
+                case '*': // multiplication
+                          stack.push(b * a);
+                          break;
+                case '/': // division
+                          stack.push(b / a);
+                          break;
+                case '^': // exponent
+                          stack.push(pow(b,a));
+                          break;
+            }
+        }
+    }
+    //returning the calculated result
+    return stack.top();
 }
 
-int isOperator(char ch) {
-   if(ch == '+'|| ch == '-'|| ch == '*'|| ch == '/' || ch == '^')
-      return 1;    //character is an operator
-   return -1;   //not an operator
-}
-
-int isOperand(char ch) {
-   if(ch >= '0' && ch <= '9')
-      return 1;   //character is an operand
-   return -1;   //not an operand
-}
-
-float operation(int a, int b, char op) {
-   //Perform operation
-   if(op == '+')
-      return b+a;
-   else if(op == '-')
-      return b-a;
-   else if(op == '*')
-      return b*a;
-   else if(op == '/')
-      return b/a;
-   else if(op == '^')
-      return pow(b,a); //find b^a
-   else
-      return INT_MIN; //return negative infinity
-}
-
-float postfixEval(string postfix) {
-   int a, b;
-   stack<float> stk;
-   string::iterator it;
-   for(it=postfix.begin(); it!=postfix.end(); it++) {
-      //read elements and perform postfix evaluation
-      if(isOperator(*it) != -1) {
-         a = stk.top();
-         stk.pop();
-         b = stk.top();
-         stk.pop();
-         stk.push(operation(a, b, *it));
-      }else if(isOperand(*it) > 0) {
-         stk.push(scanNum(*it));
-      }
-   }
-   return stk.top();
-}
-
-int main() {
-   string post = "53+62/*35*+";
-   cout << "The result is: "<<postfixEval(post);
-   return 0;
+int main()
+{
+    //we save the postfix expression to calculate in postfix_expression string
+    //string postfixExpression = "59+33^4*6/-"; //-4
+    string postfixExpression = "53+62/*35*+";  //39
+    cout<<"The answer after calculating the postfix expression is : ";
+    cout<<calculatePostfix(postfixExpression);
+    return 0;
 }
 
